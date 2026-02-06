b1bdbb43451a3be53dea07e14ca39c110e45096e
DominicDev <s0l0estudi4r@gmail.com>
2026-02-06 13:58:46 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ONE REPUBLIC';
+export let nameSongs = [
+	"COUNTING STARS", /*1*/
+	"I AIN'T WORRIED", /*2*/
+	"LOVE RUNS OUT", /*3*/
+	" AU REVOIR", /*4*/
+	"COME HOME", /*5*/
+	"WON’T STOP", /*6*/
+	"TYRANT", /*7*/
+	"APOLOGIZE", /*8*/
+	"SECRETS", /*9*/
+	"GOOD LIFE", /*10*/
+	"IF I LOSE MYSELF", /*11*/
+	"ALL THE RIGHT MOVES", /*12*/
+	"STOP AND STARE", /*13*/
+	"SOMETHING I NEED", /*14*/
+	" I LIVED", /*15*/
+	"LET'S HURT TONIGHT", /*16*/
+	"DIDN’T I", /*17*/
+	"CONNECTION", /*18*/
+	"WAKING UP", /*19*/
+	"MERCY", /*20*/
+]
